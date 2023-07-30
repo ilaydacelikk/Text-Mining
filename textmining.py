d98378b76d3533ded7bce5b2951797b981915ae4
Ilayda Celik <139812573+ilaydacelikk@users.noreply.github.com>
2023-07-30 23:13:38 +0300

    Add files via upload

---
+++ textmining.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 23:11:32 2023
+
+@author: ilayd
+"""
+
+#Ödevimde kullanacağım metni Afet İnan'ın "Atatürk Hakkında Hatıralar ve 
+#Belgeler" adlı kitabının "Kadın Hakları Üzerine" adlı bölümünden aldım.
+
+# Metini ham haliyle incelenmesi zor olacağındanstandart hale getirecek bazı işlemler uygulamamız gerek.
+# Bu işlemleri sırası ile gerçekleştirelim.
+
+
+
+metin="""
+Toplum yaşamında şüphe yok ki toplumsal gelişim, o kavmin iç bünyesinde, tarihi bir seyir izler. Fakat kavimler arasındaki temaslarla
+ dış etkiler de, zamanla kendisini hissettirir nitelik alırlar.
+ Ancak, yine tarih boyunca görülmüştür ki, sosyal devrimler çoğunlukla ulus gözünde büyük sevgi ve prestij kazanmış insanlar tarafından başarılabilir. 
+ Türk devrimi ulus tarafından çoğunluğun iyi niyeti ve hoşgörüsü
+ ile karşılanmıştır. Atatürk diyor ki: "Mühim bir vazifenin ifasında (görevin yerine getirilmesinde), benden evvel müteşebbis (girişimde bulunan),
+ millet olmuştur. Benim için şu veya bu vesile ile tehir ettiğim (ertelediğim) mühim vazifeyi, millet bana ihtar etmiş (hatırlatmış) ve yaptırmıştır.
+ Bunu milletin ruh-i müşterekindeki (ortak ruhundaki) ulviyet (yücelik) ve rüşte (olgunluğa) parlak bir misal olarak zikredeceğim.” 
+ Atatürk'ün,ulusunun yüksek yeteneklerine inancı çok büyktü. O, kadın için devrimi doğrudan doğruya kanun ve yasak yolnu hükümetin icra organlarıyla 
+ çözmek istemedi. Fakat kendi devlet başkanlığı zamanında, uygar ve siyasi haklarımızın tanınmasını sağladı ve ileri toplum düşüncelerini
+ ulusuna telkin etti. Afgan kralı Emanullah Han, Türkiye'yi ziyaretinden sonra, ülkesinde burada gördüklerini derhal uygulamak istediği zaman, 
+ isyan ile karşılaşmıştı. Atatürk'ün sabık Kral ile yaptığı sonraki görüşmesinde yanlarında bulunmuştum. Ona söylediği şu olmuştur: "Milleti ve
+ içtimai (toplumsal) zemini hazırlamadan inkılâplar yapılamaz.
+ Ancak Atatürk, kadın vatandaşa, kültür seviyesinin yükselmesini esas görmüş ve her meslekte çalışma yeteneğinin gelişmesine olanak sağlatmıştır. 
+ Bugün, ulus yaşamımız içinde, kadının düşünsel ve bedenen çalışması ile Türk toplumu uygar dünyada gerçek yerini almaktadır. Ulusal, tarihi gelişmemizi
+ cumhuriyet devrimiz kadın devrimi ile tamamladı. Bizim neslimiz bu değişmeleri yaşadı. Geleceğin kadın nesli ise, bu anıyı kutsi bir emanet olarak bilecek
+ ve nesilden nesle daima ilerlemeye tabi tutacaktır.Çünkü kadının hak ve görevlerde paralel yürümediği toplumlar, özellikle yüzyılımızda "uygar" vasfını 
+ tam olarak kazanmazlar. Biz Türk kadınları,uygar dünyada ideal kabul edilen ilkeye kavuşmuş bir nesil olarak yer almış bulunuyoruz. Esasen daima Türk köylü 
+ ve ev kadını, üreten bir uzuv olarak vatan savunmasına bilfiil katılmış, özverili bir ulus bireyi olmuştur. Bunun için Atatürk şöyle der: "Sırtı ile, 
+ kağnısı ile, kucağındaki yavrusu ile yağmur demeyip, kış demeyip, sıcak demeyip cephenin mühimmatını taşıyan hep onlar, hep o ulvi ve fedakâr, o ilahi Anadolu kadınları
+ olmuştur. Dünyanın hiçbir yerinde, hiçbir yabancı milletinde Anadolu köylü kadınının fevkinde (üstünde) kadın mesaisi zikretmek imkânı yoktur."
+ Atatürk'ün bu sözü bir teşvik, bir övünme midir? Bu soruyu, elbette ki, "Hayır" diye cevaplandırabiliriz. Çünkü Atatürk realist bir devlet adamı
+ ve ulusun yeteneklerini iyi bilen bir insandır. Bu münasebetle Dumlupınar'ın Çal köyünde o devreleri yaşamış olan kadınlarımızdan işittiğim bir 
+ sözü aktaracağım... Nisan 1955'te Başkumandanlık Meydan Muharebesi Zafer Anıtı'nın yeniden yaptırılması ve hatıralarının toplanması dolayısıyla
+ bir heyet halinde incelemelerde bulunurken bu yerlerdeki canlı tanıklarla da görüştük. Bir ihtiyar nine yanıma yaklaşmış diyordu ki, "Hani, 
+ Ankara'da Gazi Paşa'nın heykelinin yanında mermi taşıyan kadın var ya, işte o benim. Bu cümleyi yanımdaki diğer bir kadın da aynen tekrar etti.
+ Bu sözler bir gerçeğin ifadesi idi. Çünkü Ankara'daki Ulus Meydanı anıtındaki Türk köylü kadını, bir dönem tarihimizin isimsiz kahramanıdır.
+ Bu anıtta Başkumandan Mareşal Gazi Mustafa Kemal'in at üzerindeki heykelini, Türk erleri ve mermi taşıyan Türk kadınının heykelleri çevreler. 
+ İstiklal Savaşı'ndaki her Türk kadını bu anıttaki heykelde sembolleşmiştir. Ulusal varlığımız, kadın ve erkek vatandaşların topluluğu ile 
+ vücut bulur. Ancak bu topluluğun evrimi ve gelişmesi kadın ve erkeğin ortaklaşa çalışması ile mümkün olacaktır. Kadın vatandaş da, erkek 
+ vatandaş gibi, bu ülkenin evladı olarak görevlidir ve hak sahibidir. İşte, biz Türk kadınları, mesleklerimizde görev yaparken, evlatlarımızı 
+ yetiştirirken, haklarımıza sahip olduğumuzu bilerek sorumluluğumuzu anlamış vatandaşlar olmaya çalışıyoruz."""
+ 
+ 
+ #Girilen bu metinde bazı yazım yanlışları vardır. Metnin üzerinde doğru bir işlem
+ #yapabilmek için öncelikle bu yanlışları düzeltmeliyiz. Bunu da veriyi DataFrame formatına 
+ #getirerek yaparız. 
+ 
+import pandas as pd
+ 
+#İşlemlere başlamadan önce "\n" ile belirtilen kelimeleri ayırmalıyız.
+#Ayırma için .split() kullanılır. 
+
+metin.split()
+ 
+#Metnimizdeki tüm kelimeleri tek tek ayırdı ama bu istediğimiz format değil. 
+#Dolayısıyla "\n" ifadesiyle ayrılan cümleleri ayırt etmemiz lazım.
+
+metin2= metin.split("\n") 
+print(metin2)   
+len(metin2) 
+ 
+#Her cümlemiz tırnak işaretiyle ayrıldı.  
+# Metnimizin satır uzunluğunu da "len" komutuyla 32 bulduk. 
+#Ama veri setimizin başında boş satır bulundurduğumuz için 
+#onu silmeliyiz. 
+
+metin2=metin2[1:len(metin2)] 
+print(metin2) 
+len(metin2) 
+
+#Bu işlemle üstteki boşluğu sildik ve satır uzunluğumuz 
+#31 oldu. 
+ 
+#Artık verimiz DataFrame formatına çevrilebilir. 
+metin_df=pd.DataFrame(metin2,columns=["Atatürk ve Kadın Hakları"])
+print(metin_df) 
+ 
+#Kolay işlem yapabilmek için bu hale getirdik.Şimdi veri 
+#içerisindeki yanlışlıklar giderilebilir. 
+
+#Başlangıç olarak tüm kelimeleri ortak bir formata 
+#(hepsinin küçük ya da büyük harf olması) getirelim.
+
+#Verimizi kopyalayalım işleme öyle devam edelim. 
+veri=metin_df.copy() 
+
+
+veri=veri["Atatürk ve Kadın Hakları"].apply(lambda x:" ".join(x.lower() for x in x.split()))
+print(veri)
+ 
+#Verimizde inceleme yaparken küçük-büyük harfe toleransı olmadığı için tüm kelimeleri 
+#aynı formatta yazdık yani küçük harflerle yazdık.[ Atamızın ismi de dahil :( ]  
+ 
+#Noktalama işaretlerini silelim. 
+#bu amaçla .replace("[^\w\s"]) kullanılır.
+veri=veri.str.replace("[^\w\s]","")
+print(veri) 
+ 
+#Şimdide verideki sayıları silelim. 
+veri=veri.str.replace("\d","")
+print(veri)   #1955 yazıyordu o silindi.
+ 
+#türkçede stopwords ifadeleri (ise,mü,da,...) anlamsızdır. 
+#Dolayısıyla bu ifadelerin veriden temizlenmesi gerekir. bu amaçla
+#"nltk" kütüphanesinin kurulması gerekir.
+
+!pip install nltk
+import nltk 
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+stp = stopwords.words("turkish")  
+print(stp) #türkçedeki tüm anlamsız kelimeleri çıkarttı. 
+ 
+veri=pd.DataFrame(veri,columns=["Atatürk ve Kadın Hakları"])
+veri=veri["Atatürk ve Kadın Hakları"].apply(lambda x: " ".join(x for x in x.split() if x not in stp))
+print(veri) 
+
+# Part of Speech Tagging (POS): metinde bulunan kelimelerin sıfat, zarf isim olma
+# durumlarına göre tespit etmek için kullanılır.
+nltk.download("averaged_perceptron_tagger")
+print(veri["Atatürk ve Kadın Hakları"][2]).tags
+veri["Atatürk ve Kadın Hakları"].apply(lambda x: TextBlob(x).tags) 
+# Tüm kelimelerin türünü öğrenmiş olduk.
+
+ 
+#Bir sonraki aşama kelimeleri kökler haline getirme işlemi.
+#Satırlar boyunca çalışacak for döngüsü:
+#Örneğin koş ve koşmak gibi iki gelimenin kökü de aynıdır. 
+#Dolayısıyla bu iki kelime aynı köke sahip olacak şekilde ayrılmalıdır.
+#Bu amaçlar textblob kütüphanesi kullanılır. 
+
+!pip install textblob
+from textblob import Word
+nltk.download('omw-1.4')
+type(veri)
+
+veri = pd.DataFrame(veri,columns =["Atatürk ve Kadın Hakları"])
+veri = veri["Atatürk ve Kadın Hakları"].apply(lambda x: " ".join(Word(i).lemmatize()for i in x.split()))
+print(veri) 
+ 
+#lemmatize ile köklerine ayırdık.
+#verimizi rakamlardan,noktalama işaretlerinden ve yanlış kelimelerden
+#ayırdıktan sonra diğer işlemlere geçebiliriz.
+
+
+      #Metinler üzerinde matematiksel işlemler ve özetlemeler
+#Veri setinin ilk halini düşünelim.
+print(metin_df)
+
+#Karakter sayısı
+metin_df["nchar"] = metin_df["Atatürk ve Kadın Hakları"].str.len()
+print(metin_df) 
+#Bunu çalıştırdığımızda her satırda kaç karakter olduğunu görürüz. 
+#Mesela ilk satırda 133 karakter varken, ikinci satırda 63 tane var... 
+
+#Rakam sayısı
+metin_df["nnum"] = metin_df["Atatürk ve Kadın Hakları"].apply(lambda x: len([x for x in x.split() if x .isdigit()]))
+print(metin_df) 
+#tüm rakamları yukarda çıkardığımız için rakam sayıları her satırda 0 çıktı.
+
+#Kelime sayısı
+metin_df["nword"] = metin_df["Atatürk ve Kadın Hakları"].apply(lambda x: len(str(x).split(" ")))
+print(metin_df)
+#her satırda kaç kelime olduğunu gösteriyor. Mesela ilk satırda 19 kelime
+#ikinci satırda 9 kelime var...
+
+print(veri)
+
+data2=veri.copy()
+print(data2)
+
+#Terim frekanslarını hesaplayalım.
+data2=pd.DataFrame(data2) 
+frekanslar = data2.apply(lambda x:pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
+print(frekanslar)
+frekanslar = ["words","counts"]
+
+# Örneğin 10 den fazla tekrarlanan kelimeler nelermiş buna bir bakalım.
+from nltk import word_tokenize, pos_tag, ne_chunk 
+nltk.download("maxent_ne_chunker")
+nltk.download("words")
+
+
+#Daha iyi incelemek için kelimelerimizi görselleştirelim.
+!pip install wordcloud
+import pandas as pd
+import numpy as np
+from os import path 
+from PIL import Image 
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib.pyplot as mlt
+
+#Verimizden spontane bir cümle seçelim ve onu grafiğe dökelim.
+
+yeniveri= "Biz Türk kadınları uygar dünyada ideal kabul edilen ilkeye kavuşmuş bir nesil olarak yer almış bulunuyoruz"
+
+#önce boş word cloud yaratalım. 
+wrdcl=WordCloud().generate(yeniveri)
+mlt.imshow(wrdcl, interpolation = "bilinear") #grafiğimiz çıktı
+mlt.axis("off") #resetlendi 
+
+#Grafiği kaydettirmek icin 
+wrdcl.to_file("graph.jpg")
+
+
+wrdcl = WordCloud(max_font_size = 30, max_words = 20, background_color = "white").generate(yeniveri)
+
+#tonunu, kelimelerin büyüklüklerini ve grafiğin
+#arkaplanını değiştirdik 
+
+mlt.imshow(wrdcl, interpolation = "bilinear")
+mlt.show()
+
